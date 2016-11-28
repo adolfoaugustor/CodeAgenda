001708031d0500fa268a9d039c083b62784e0bc5
AdolfoAugustoR <adolfoaugustor@gmailcom>
2016-11-28 03:22:40 +0000

    iniciando o projeto lumen /  routes e controllers

---
+++ app/Http/Controllers/ExampleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace CodeAgenda\Http\Controllers;
+
+class ExampleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    //
+}
---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: swr2d2
+ * Date: 27/11/2016
+ * Time: 23:41
+ */
+
+namespace CodeAgenda\Http\Controllers;
+
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        return "Olá Mundo";
+    }
+}
