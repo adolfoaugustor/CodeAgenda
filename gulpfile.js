e6df74db91b0f05296af2dda5e9f24f1ed744cdc
AdolfoAugustoR <adolfoaugustor@gmailcom>
2016-12-05 02:11:17 +0000

    bug fonts

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var elixir = require('laravel-elixir'),
 
 elixir(function(mix){
     mix.copy(bowerDir + 'bootstrap/fonts', 'public/fonts')
-        .copy(bowerDir + 'fontawesome/fonts', 'public/fonts')
+        .copy(bowerDir + 'font-awesome/fonts', 'public/fonts')
 
         .copy(bowerDir + 'bootstrap/dist/js/bootstrap.js', 'resources/assets/js')
         .copy(bowerDir + 'jquery/dist/jquery.js', 'resources/assets/js')
