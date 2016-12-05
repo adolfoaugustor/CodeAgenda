2af3f4f647f05b80526bbaea8da3ee52a62de1d9
AdolfoAugustoR <adolfoaugustor@gmailcom>
2016-12-05 01:52:53 +0000

    listagem de pessoas e telefones / exibir numeros que a letra for selecionado e view de numeros e nomes

---
+++ resources/views/agenda.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+        <meta charset="UTF-8">
+        <title>Aulas tutoriais</title>
+    </head>
+    <body>
+        <?php
+        require('./inc/Config.inc.php');
+
+        ?>
+</body>
+</html>
